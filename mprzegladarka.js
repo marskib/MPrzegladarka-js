773c4155e98c02e050a5cba37f2670cffa16d47a
marskib <tata.marcina@autyzmsoft.pl>
2019-11-14 18:49:03 +0100

    cd

--- mprzegladarka.js
+++ mprzegladarka.js
@@ -1,15 +1,15 @@
 "use strict";
 
-let obrazki = ["bluzka", "chleb", "choinka", "cukierki", "czajnik", "czekolada", "dziewczynka", "długopis", "grzebień", "jabłko",
+const obrazki = ["bluzka", "chleb", "choinka", "cukierki", "czajnik", "czekolada", "dziewczynka", "długopis", "grzebień", "jabłko",
     "klocki", "kot", "kredki", "krzesło", "książka", "lampa", "miotła", "miś", "myszka", "młotek", "nożyczki", "nóż", "odkurzacz", "okno",
     "okulary", "ołówek", "pies", "pilot", "piłka", "poduszka", "pomidory", "ręcznik", "spodnie", "słodycze", "talerz", "widelec", "wieża",
     "zebra", "zegar", "łyżka"
 ];
 
 let CURR_NROBR = null; //numer pokazywanego obrazka
-let pctArea = null; //miejsce na planszy (div) na obrazek
-let bWpieriod = null; //klawisz bWpieriod
-let bNazad = null; //klawisz bNazad
+let pctArea = null;    //miejsce na planszy (div) na obrazek
+let bWpieriod = null;  //klawisz bWpieriod
+let bNazad = null;     //klawisz bNazad
 const PCT_DELAY = 1000; //opoznienie w pokazywaniu obrazka
 
 //-----------------------------------------------------//
@@ -27,20 +27,25 @@ function Inicjacja() {
     //
     CURR_NROBR = getRandomIntInclusive(0, obrazki.length - 1);
     ustawObrazek();
+    pokazKlawisze(1.0*PCT_DELAY);
+}
+
+function pokazKlawisze(delay) {
+    setTimeout(()=>document.getElementById("btnsArea").style.visibility = "visible" , delay);
 }
 
 function ustawObrazek() {
     //---------------------------    
     //Funckcja :
     //0.Czysci stary obrazek (efekciarstwo)
+    //1.Pokazuje nowy obrazek
     //2.Odgrywa nazwę obrazka
     //3.Ustawia onclik na obrazku, tak zeby mozliwe bylo jego JEDNORAZOWE odegranie
     //---------------------------
     //czyszczenie starego (dla efektu)
     pctArea.style.backgroundImage = "url(zasoby/tlo.webp)";
-    //losowanie obrazka:
-    var pctName = obrazki[CURR_NROBR];
     //Pokazanie po lekkim opoznieniu (efekciarstwo):
+    var pctName = obrazki[CURR_NROBR];
     setTimeout(() => (pctArea.style.backgroundImage = "url(zasoby/" + pctName + ".webp)"), PCT_DELAY);
     //jednorazowy listener i pointer na obrazku:
     setTimeout(() => (pctArea.style.cursor = "pointer"), 2 * PCT_DELAY);
