8f68e5ed9a9b4f2ae9f845439f853c252662c9e4
marskib <tata.marcina@autyzmsoft.pl>
2019-11-14 15:47:19 +0100

    cd

--- mprzegladarka.js
+++ mprzegladarka.js
@@ -10,7 +10,7 @@ let CURR_NROBR = null; //numer pokazywanego obrazka
 let pctArea = null; //miejsce na planszy (div) na obrazek
 let bWpieriod = null; //klawisz bWpieriod
 let bNazad = null; //klawisz bNazad
-const PCT_DELAY = 1200; //opoznienie w pokazywaniu obrazka
+const PCT_DELAY = 1000; //opoznienie w pokazywaniu obrazka
 
 //-----------------------------------------------------//
 window.onload = Inicjacja; //--------------------------//
@@ -46,7 +46,7 @@ function ustawObrazek() {
     setTimeout(() => (pctArea.style.cursor = "pointer"), 2 * PCT_DELAY);
     pctArea.onclick = function () { odegrajPlik(pctName + ".ogg", 0); pctArea.onclick = null; pctArea.style.cursor = "auto"; }
     //
-    odegrajPlik(pctName + ".ogg", PCT_DELAY);
+    odegrajPlik(pctName + ".ogg", 2*PCT_DELAY);
 }
 
 function dajNextPicture() {
